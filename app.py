79a439440e88584aa38458c4242fae393328d039
Muthuarun-M <muthuarun884@gmail.com>
2026-02-11 11:47:03 +0530

    Updated files

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template, request
+from controller.database import db
+from controller.confige import Confige
+from controller.model import *
+
 app = Flask(__name__)
+app.config.from_object(Confige)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+    if Role.query.count() == 0:
+        student = Role(name='student')
+        admin = Role(name='Admin')
+        staff = Role(name='Staff')
+        
+        db.session.add_all([student, admin, staff]) 
+        db.session.commit()
+        
 
 @app.route('/')
 def hello_world():
--- controller/model.py
+++ controller/model.py
@@ -1,18 +1,19 @@
 from controller.database import db
+
 class User(db.Model):
-    __Tablename__ = 'users'
+    __Tablename__ = 'user'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
 
 class Role(db.Model):
-    __Tablename__ = 'roles'
+    __Tablename__ = 'role'
     role_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
 
 class UserRole(db.Model):
-    __Tablename__ = 'user_roles'
+    __Tablename__ = 'user_role'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.role_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.role_id'), nullable=False)
